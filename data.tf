f10c75412fb6bd325f7d50d3d5d88ec3554842fa
shashimal <shashimald@gmail.com>
2025-11-01 14:10:53 +0800

    Setup the application

---
+++ data.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_route53_zone" "zone" {
+  name = local.app_domain
+}
